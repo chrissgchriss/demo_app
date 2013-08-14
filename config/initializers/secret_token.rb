662d295d60351bc893d7f9fe9a8a5a6020881dc8
chrissgchriss <chrissgchriss@gmail.com>
2013-08-14 16:02:20 -0700

    Initial commit gbs demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '639dc19fd925d9df9c6a91c74a2776095735c802e278007d8576a1735962f896fff1d97004d126d3a3bd9a66d11a403aa2e4f54ca325dfbbe2ed5c7d01d500e3'
